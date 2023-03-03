6a5440a3df264eb067b1294b485ce76b8798eeb3
vignesh-kundar <vigneshkundar125@gmail.com>
2023-03-04 00:09:28 +0530

    add comments for users

--- Vtrack_2.ino
+++ Vtrack_2.ino
@@ -2,10 +2,16 @@
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 TinyGPSPlus gps;  // The TinyGPS++ object
-SoftwareSerial ss(4, 5); // The serial connection to the GPS device
-const char* ssid = "INTERNET"; //ssid of your wifi
-const char* password = "8197444820"; //password of your wifi
-float latitude , longitude;
+SoftwareSerial ss(4, 5); // The serial connection to the GPS device:: actually it represents the pins but refer the pins , go through the pin diagram and follow it up.
+
+// WIFI PASSWORD AND SSID
+const char* ssid = "Enter_your_wifi_ssid_here"; //ssid of your wifi
+const char* password = "Password_paadle_anna";  //password of your wifi
+
+//LATITUDE AND LONGITUDE DATA WILL BE STORED IN THIS VARIABLE
+float latitude , longitude; //YOU CANNOT USE THIS BUT SHOULD BE CONVERTED TO STRING
+
+// EXTRA INFORMATION
 int year , month , date, hour , minute , second;
 String date_str , time_str , lat_str , lng_str;
 int pm;
@@ -34,9 +40,10 @@ void setup()
 
 void loop()
 {
-  while (ss.available() > 0) //while data is available
-    if (gps.encode(ss.read())) //read gps data
+  while (ss.available() > 0)    //while data is available
+    if (gps.encode(ss.read()))  //read gps data
     {
+      // IN THIS PART YOU WILL BE GETTING THE LATITUDE AND LONGITUDE IS ALL CREDENTIALS ARE MET
       if (gps.location.isValid()) //check whether gps location is valid
       {
         Serial.println();
@@ -45,6 +52,11 @@ void loop()
         longitude = gps.location.lng();
         lng_str = String(longitude , 6);//longitude location is stored in a string
       }
+      
+      //NOTE 
+      
+      //IN THE ABOVE CODE `lat_str` and `lng_str` THESE TWO CAN BE USED TO UPDATE THE DATA IN THE CLOUD
+      
       if (gps.date.isValid()) //check whether gps date is valid
       {
         date_str = "";
@@ -53,24 +65,24 @@ void loop()
         year = gps.date.year();
         if (date < 10)
           date_str = '0';
-        date_str += String(date);// values of date,month and year are stored in a string
+        date_str += String(date);   // values of date,month and year are stored in a string
         date_str += " / ";
 
         if (month < 10)
           date_str += '0';
-        date_str += String(month); // values of date,month and year are stored in a string
+        date_str += String(month);  // values of date,month and year are stored in a string
         date_str += " / ";
         if (year < 10)
           date_str += '0';
-        date_str += String(year); // values of date,month and year are stored in a string
+        date_str += String(year);   // values of date,month and year are stored in a string
       }
-      if (gps.time.isValid())  //check whether gps time is valid
+      if (gps.time.isValid())       //check whether gps time is valid
       {
         time_str = "";
         hour = gps.time.hour();
         minute = gps.time.minute();
         second = gps.time.second();
-        minute = (minute + 30); // converting to IST
+        minute = (minute + 30);     // converting to IST -> INDIA STANDARD TIME
         if (minute > 59)
         {
           minute = minute - 60;
@@ -78,8 +90,8 @@ void loop()
         }
         hour = (hour + 5) ;
         if (hour > 23)
-          hour = hour - 24;   // converting to IST
-        if (hour >= 12)  // checking whether AM or PM
+          hour = hour - 24;        // converting to IST
+        if (hour >= 12)            // checking whether AM or PM
           pm = 1;
         else
           pm = 0;
@@ -101,13 +113,22 @@ void loop()
           time_str += " AM ";
       }
     }
+  
+  
+  //ONCE YOU GET THE LAT_STRING AND LONG_STRING
+  // ITS UP TO YOU WHERE YOU PUT THE LATITUDE AND LONGITUDE INTO.
  
+  
+ // YOUL GET ONE IP ADDRESS YOU HAVE TO SEE THAT IN THE ARDUINO APPLICATION :)
+ // ASK HELP FROM EC PPL IF IAM UNABLE TO THERE YOU SHOULD GET IP ADRESS IN SPECIFIC PORT IF ITS NOT COMMING CHECK OUT SERIAL MONITOR ONCE CONNECTED AND ALSO USE RESET BUTTON IN THE NODE MCU IT WILL RESTART THE PROCESS. 
  WiFiClient client = server.available(); // Check if a client has connected
   if (!client)
   {
     return;
   }
   // Prepare the response
+  // BELOW CODE IS TO RENDER A WEB PAGE WHICH WILL BE CREATED LOCALLY
+  // HINT PASS THE DATA THROUGH ONE API TO STORE THE LOCATION INFORMATION :)
   String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n <!DOCTYPE html> <html> <head> <title>GPS DATA</title> <style>";
   s += "a:link {background-color: YELLOW;text-decoration: none;}";
   s += "table, th, td </style> </head> <body> <h1  style=";
@@ -126,7 +147,6 @@ void loop()
   s += "</td></tr> <tr> <th>Time</th> <td ALIGN=CENTER >";
   s += time_str;
   s += "</td>  </tr> </table> ";
-
   s += "<a href="https://www.google.com/maps/place/12%C2%B049'25.5%22N+74%C2%B050'35.9%22E/@12.8238907,74.842751,17.78z/">Click here to reddirect to Google Maps</a>"
   s += "</body> </html>";
 
